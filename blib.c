e47ceadcd1f45c196cc795753bb34c996ae9e40d
范程 <21371286@buaa.edu.cn>
2023-02-26 18:50:54 +0800

    first c exercise

--- blib.c
+++ blib.c
@@ -1,11 +1,18 @@
 #include <blib.h>
 
 size_t strlen(const char *s) {
-	panic("please implement");
+	size_t length;
+	for(length=0;s[length]!='\0';length++);
+	return length;
 }
 
 char *strcpy(char *dst, const char *src) {
-	panic("please implement");
+	char *res=dst;
+	while(*src){
+		*dst++=*src++;
+	}
+	*dst='\0';
+	return res;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -18,11 +25,28 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-	panic("please implement");
+	char *res = dst;
+	size_t i,j;
+	for(i=0;dst[i]!='\0';i++);
+	for(j=0;src[j]!='\0';j++,i++)
+	{
+		dst[i]=src[j];
+	}
+	dst[i]='\0';
+	return res;
 }
 
 int strcmp(const char *s1, const char *s2) {
-	panic("please implement");
+	size_t i;
+	int ans;
+	for(i=0;s1[i]!='\0'&&s2[i]!='\0'&&s1[i]==s2[i];i++);
+	if(s1[i]==s2[i])
+		ans=0;
+	else if(s1[i]<s2[i])
+		ans=-1;
+	else
+		ans=1;
+	return ans;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -40,7 +64,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-	panic("please implement");
+	const unsigned char uc = c;
+	unsigned char *su;
+	for(su = s;0 < n;++su,--n)
+		*su = uc;
+	return s;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
@@ -53,5 +81,15 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-	panic("please implement");
+	const unsigned char *su1=s1;
+	const unsigned char *su2=s2;
+	int i;
+	for(i=0;i<n;i++){
+		if(su1[i]<su2[i])
+			return -1;
+		else if(su1[i]>su2[i])
+			return 1;
+	}
+	return 0;
+
 }
